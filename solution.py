a637c2a5bd572e132f5a8d8c8a02f8a7fa919d9c
Kooreis <kooreis.official@gmail.com>
2024-01-06 18:28:02 -0600

    Added a function to detect if a cycle exists in the linked list and to find the start of the cycle. It uses the Floyd's cycle-finding algorithm.

--- solution.py
+++ solution.py
@@ -1,4 +1,14 @@
-class Node:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+def detectCycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None
+
+    while head != slow:
+        head = head.next
+        slow = slow.next
+    return head
