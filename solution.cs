0301888ae9bae3655119a8cd95269ae3d0252545
Kooreis <kooreis.official@gmail.com>
2024-01-06 18:28:02 -0600

    This update introduces the LinkedList class, which represents a linked list. The LinkedList class has a Head property, which points to the first node in the list, and a DetectCycle method, which uses Floyd's cycle-finding algorithm to detect a cycle in the list and return the node where the cycle starts.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,28 @@
-using System;
-
-public class Node
+public class LinkedList
 {
-    public int Value { get; set; }
-    public Node Next { get; set; }
+    public Node Head { get; set; }
+
+    public Node DetectCycle()
+    {
+        Node slow = Head;
+        Node fast = Head;
+
+        while (fast != null && fast.Next != null)
+        {
+            slow = slow.Next;
+            fast = fast.Next.Next;
+
+            if (slow == fast)
+            {
+                Node slow2 = Head;
+                while (slow2 != slow)
+                {
+                    slow = slow.Next;
+                    slow2 = slow2.Next;
+                }
+                return slow2;
+            }
+        }
+        return null;
+    }
 }
