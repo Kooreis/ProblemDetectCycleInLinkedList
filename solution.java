47930b3069bc8e86c239edcee9e0f8fc9e872782
Kooreis <kooreis.official@gmail.com>
2024-01-06 18:28:02 -0600

    This update introduces the LinkedList class with a head node and a push method. The push method is used to add new nodes to the LinkedList.

--- solution.java
+++ solution.java
@@ -1,11 +1,9 @@
-import java.util.HashSet;
+class LinkedList {
+    Node head;
 
-class Node {
-    int data;
-    Node next;
-
-    Node(int data) {
-        this.data = data;
-        next = null;
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
     }
 }
